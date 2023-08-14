aff17ae19b7b04c1c42bfada8dc01957ec6f6e72
Tomas Benfica <tomasrbenfica@outlook.com>
2023-08-14 14:42:49 -0300

    HeroRestaurant desktop e responsivo finalizados

--- src/pages/Restaurant/HeroRestaurant/HeroRestaurant.tsx
+++ src/pages/Restaurant/HeroRestaurant/HeroRestaurant.tsx
@@ -1,6 +1,7 @@
 
 import styles from './HeroRestaurant.module.css'
 import percentage from '../../../assets/percentage.png'
+import star from '../../../assets/star.png'
 
 const HeroComponent: React.FC<{ name:string, location: string, image:string, rating:number, deliveryTime:number }> = (props) => {
   return (
@@ -16,7 +17,10 @@ const HeroComponent: React.FC<{ name:string, location: string, image:string, rat
           <p>{props.location}</p>
           <div className={styles.heroTitleSubinfos}>
             <div className={styles.heroTitleSubinfosInfo}>
-              <p className={styles.heroTitleSubinfosInfoRating}>{props.rating}</p>
+              <div className={styles.heroTitleSubinfosInfoRating}>  
+                <img src={star} alt="" />
+                <p>{props.rating}</p>
+              </div>
               <p>100+ ratings</p>
             </div>
 
@@ -33,20 +37,49 @@ const HeroComponent: React.FC<{ name:string, location: string, image:string, rat
               <p>$ 200</p>
               <p>Cost for two</p>
             </div>
-
           </div>
         </div>
 
+        {/* <div className={styles.heroTitle}>
+          <h1>{props.name}</h1>
+          <p>{props.location}</p>
+          <div className={styles.heroTitleSubinfos}>
+            <div className={styles.heroTitleSubinfosInfo}>
+              <div className={styles.heroTitleSubinfosInfoRating}>  
+                <img src={star} alt="" />
+                <p>{props.rating}</p>
+              </div>
+              <p>100+ ratings</p>
+            </div>
+
+            <div className={styles.heroTitleSubinfosBar} />
+
+            <div className={styles.heroTitleSubinfosInfo}>
+              <p>{props.deliveryTime}</p>
+              <p>Delivery Time</p>
+            </div>
+
+            <div className={styles.heroTitleSubinfosBar} />
+
+            <div className={styles.heroTitleSubinfosInfo}>
+              <p>$ 200</p>
+              <p>Cost for two</p>
+            </div>
+          </div>
+        </div> */}
+
 
         <div className={styles.heroOffers}>
-          <h1 className={styles.heroOffersTitle}>Offers</h1>
+          <h3 className={styles.heroOffersTitle}>Offers</h3>
           <div className={styles.heroOffersContainer}>
-            <img src={percentage} alt="" />
-            <p>50% off up to $100 | Use code TRYNEW</p>
-          </div>
-          <div className={styles.heroOffersContainer}>
-            <img src={percentage} alt="" />
-            <p>20% off | Use code PARTY</p>
+            <div className={styles.heroOffersContainerItem}>
+              <img src={percentage} alt="" />
+              <p>50% off up to $100 | Use code TRYNEW</p>
+            </div>
+            <div className={styles.heroOffersContainerItem}>
+              <img src={percentage} alt="" />
+              <p>20% off | Use code PARTY</p>
+            </div>
           </div>
         </div>
 
